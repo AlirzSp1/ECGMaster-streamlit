2122208518f0c509893929b80e5b22bc70d5f601
Alireza-NUC <alirzsp77@gmail.com>
2025-06-08 18:20:41 +0330

    delete test file

--- test1.py
+++
@@ -1,8 +0,0 @@
-from streamlit_utils.firestore_utils import init_firestore
-
-db = init_firestore()
-
-docs = db.collection('ecg_data_1').stream()
-
-ecg_id_list = [doc.id for doc in docs]
-print(ecg_id_list)
